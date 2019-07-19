68fa99d25dea5491ea78f40ed657ed1f6d0a42d5
Max Troughear <max.troughear@gmail.com>
2019-07-20 02:10:19 +1200

    Big change, new design, using bootstrap studio. Api security, etc working

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const auth = require('./auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index-test', { title: 'Car Tracking', content: 'Some new content' });
+  res.render('index', { title: 'Car Tracking', content: 'Some new content' });
 });
 
 module.exports = router;
