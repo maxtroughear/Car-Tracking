1c4f5fa8772f7124a610139ba5d43f60f158195f
Max Troughear <max.troughear@gmail.com>
2019-08-01 23:33:52 +1200

    Changed mongodb to work on local server

--- config/index.js
+++ config/index.js
@@ -3,7 +3,8 @@ const config = {};
 config.appPort = 3004;
 
 config.mongodb = {
-    uri: 'mongodb+srv://admin:pZlVuvhCVRY5oVoS@cluster0-vztbn.mongodb.net/cartracking?retryWrites=true&w=majority'
+    //uri: 'mongodb+srv://admin:pZlVuvhCVRY5oVoS@cluster0-vztbn.mongodb.net/cartracking?retryWrites=true&w=majority'
+    uri: 'mongodb://localhost/cartracking?retryWrites=true&w=majority'
 };
 
 config.session = {
